e04993d24aa69f317610d8926ef7ff16b7115389
Malik Ahmed <olemalik@gmail.com>
2021-10-30 16:57:57 +0530

    Structural changes

--- js/signIn.js
+++ js/signIn.js
@@ -1,7 +1,7 @@
 async function SignIn(){
-    debugger
+    
     var param = convertFormToJson($("#frmSignIn"));
-
+    debugger
     if(!param.username && !param.password){
         alert("Credentials cannot be empty!");
         return false;
@@ -11,7 +11,7 @@ async function SignIn(){
         var result = await postApiAjaxCall("Authentication",param) ;
 
         let jsonstring = JSON.stringify({'token':"3fa85f64-5717-4562-b3fc-2c963f66afa6", "userid " : 1});
-        setLocaStorage("userToken",jsonstring);
+        setLocalStorage("userToken",jsonstring);
 
     } catch(err) {
         console.log(err);
